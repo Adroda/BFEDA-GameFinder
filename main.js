1506ee40d313d0185947966599b9cfef3f344382
adroda <adrianchi_91@hotmail.com>
2022-06-09 00:27:55 -0300

    Auto Slider

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// CAROUSEL
 const carouselSlide = document.querySelector(
     '.carouselContainer__carouselSlide'
 );
@@ -5,6 +6,8 @@ const carouselSlide = document.querySelector(
 const carouselImages = document.querySelectorAll(
     '.carouselContainer__carouselSlide img'
 );
+
+const carouselBottomWrapper = document.querySelector('.carouselSlider');
 
 //Buttons
 const prevBtn = document.querySelector('.previous');
@@ -15,10 +18,6 @@ const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 
-const login = () => {
-    window.location.replace('components/home/index.html');
-};
-
 //Button Listeners
 
 nextBtn.addEventListener('click', () => {
@@ -26,6 +25,16 @@ nextBtn.addEventListener('click', () => {
     carouselSlide.style.transition = 'transform 0.7s ease-in-out';
     counter++;
     carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+
+    Array.from(carouselBottomWrapper.children).forEach((element, index) => {
+        if (index + 1 !== counter) {
+            element.classList.remove('carouselSlider__btn--active');
+        } else {
+            Array.from(carouselBottomWrapper.children)[
+                counter - 1
+            ].classList.add('carouselSlider__btn--active');
+        }
+    });
 });
 
 prevBtn.addEventListener('click', () => {
@@ -33,6 +42,16 @@ prevBtn.addEventListener('click', () => {
     carouselSlide.style.transition = 'transform 0.7s ease-in-out';
     counter--;
     carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+
+    Array.from(carouselBottomWrapper.children).forEach((element, index) => {
+        if (index + 1 !== counter) {
+            element.classList.remove('carouselSlider__btn--active');
+        } else {
+            Array.from(carouselBottomWrapper.children)[
+                counter - 1
+            ].classList.add('carouselSlider__btn--active');
+        }
+    });
 });
 
 carouselSlide.addEventListener('transitionend', () => {
@@ -40,10 +59,55 @@ carouselSlide.addEventListener('transitionend', () => {
         carouselSlide.style.transition = 'none';
         counter = carouselImages.length - 2;
         carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+        Array.from(carouselBottomWrapper.children)[5].classList.add(
+            'carouselSlider__btn--active'
+        );
     }
     if (carouselImages[counter].className === 'carouselSlide__firstClone') {
         carouselSlide.style.transition = 'none';
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+        Array.from(carouselBottomWrapper.children)[0].classList.add(
+            'carouselSlider__btn--active'
+        );
     }
 });
+
+// Bottom Slider
+
+carouselBottomWrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) return;
+    Array.from(carouselBottomWrapper.children).forEach((element) => {
+        if (element !== event.target) {
+            element.classList.remove('carouselSlider__btn--active');
+        } else {
+            carouselSlide.style.transition = 'transform 0.7s ease-in-out';
+            counter =
+                Array.from(carouselBottomWrapper.children).indexOf(element) + 1;
+            carouselSlide.style.transform =
+                'translateX(' + -size * counter + 'px)';
+        }
+    });
+    event.target.classList.add('carouselSlider__btn--active');
+});
+
+setInterval(() => {
+    carouselSlide.style.transition = 'transform 0.7s ease-in-out';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+    Array.from(carouselBottomWrapper.children).forEach((element, index) => {
+        if (index + 1 !== counter) {
+            element.classList.remove('carouselSlider__btn--active');
+        } else {
+            Array.from(carouselBottomWrapper.children)[
+                counter - 1
+            ].classList.add('carouselSlider__btn--active');
+        }
+    });
+}, 3000);
+
+// LOGIN
+const login = () => {
+    window.location.replace('components/home/index.html');
+};
